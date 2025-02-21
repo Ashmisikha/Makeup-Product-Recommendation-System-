257cf5cc491ffa56eefe8873cec21a08a3ad5620
ashmisikha piri <149249477+Ashmisikha@users.noreply.github.com>
2025-02-22 00:30:49 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,32 @@
+public class Product {
+    private String name;
+    private String category;
+    private String shade;
+    private String brand;
+
+    public Product(String name, String category, String shade, String brand) {
+        this.name = name;
+        this.category = category;
+        this.shade = shade;
+        this.brand = brand;
+    }
+
+    public String getCategory() { return category; }
+    public String getShade() { return shade; }
+}
+public class Product {
+    private String name;
+    private String category;
+    private String shade;
+    private String brand;
+
+    public Product(String name, String category, String shade, String brand) {
+        this.name = name;
+        this.category = category;
+        this.shade = shade;
+        this.brand = brand;
+    }
+
+    public String getCategory() { return category; }
+    public String getShade() { return shade; }
+}
